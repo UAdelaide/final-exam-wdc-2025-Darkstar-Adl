d25deb8d4d7afbbd7dfa5f961b3a335df43e07a3
darkstar-adl <a1926618@adelaide.edu.au>
2025-06-20 11:06:02 +0000

    [automatic commit] 2025-06-20 11:06:02 UTC 122.150.235.86

--- part1/Part5-Rows.sql
+++ part1/Part5-Rows.sql
@@ -9,15 +9,15 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 ("examsuccess99", "winning@exam.com", "hashed99", "owner");
 
 INSERT INTO Dogs (name, size, owner_id) VALUES
-("Max", "medium", (SELECT dog_id FROM Users WHERE username = "alice123")),
-("Bella", "small", (SELECT dog_id FROM Users WHERE username = "carol123")),
-("Scooby", "large", (SELECT dog_id FROM Users WHERE username = "examsuccess99")),
-("Knine", "small", (SELECT dog_id FROM Users WHERE username = "examsuccess99")),
-("Maxwell", "medium", (SELECT dog_id FROM Users WHERE username = "exampasser123"));
+("Max", "medium", (SELECT user_id FROM Users WHERE username = "alice123")),
+("Bella", "small", (SELECT user_id FROM Users WHERE username = "carol123")),
+("Scooby", "large", (SELECT user_id FROM Users WHERE username = "examsuccess99")),
+("Knine", "small", (SELECT user_id FROM Users WHERE username = "examsuccess99")),
+("Maxwell", "medium", (SELECT user_id FROM Users WHERE username = "exampasser123"));
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
-((SELECT user_id FROM Dogs WHERE name = "Max"), "2025-06-10 08:00:00", 30, "Parklands", "open"),
-((SELECT user_id FROM Dogs WHERE name = "Bella"), "2025-06-10 09:30:00", 45, "Beachside Ave", "accepted"),
-((SELECT user_id FROM Dogs WHERE name = "Scooby"), "2025-06-20 08:30:00", 45, "Starting Ave", "accepted"),
-((SELECT user_id FROM Dogs WHERE name = "Knine"), "2025-06-21 01:30:00", 60, "Middle St", "open"),
-((SELECT user_id FROM Dogs WHERE name = "Maxwell"), "2025-06-21 16:30:00", 30, "Final Blvd", "open");
+((SELECT dog_id FROM Dogs WHERE name = "Max"), "2025-06-10 08:00:00", 30, "Parklands", "open"),
+((SELECT dog_id FROM Dogs WHERE name = "Bella"), "2025-06-10 09:30:00", 45, "Beachside Ave", "accepted"),
+((SELECT dog_id FROM Dogs WHERE name = "Scooby"), "2025-06-20 08:30:00", 45, "Starting Ave", "accepted"),
+((SELECT dog_id FROM Dogs WHERE name = "Knine"), "2025-06-21 01:30:00", 60, "Middle St", "open"),
+((SELECT dog_id FROM Dogs WHERE name = "Maxwell"), "2025-06-21 16:30:00", 30, "Final Blvd", "open");
