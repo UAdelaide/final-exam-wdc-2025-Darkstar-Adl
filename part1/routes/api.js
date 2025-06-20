d579fc614a308589dbe5809823490cc365c9ba25
darkstar-adl <a1926618@adelaide.edu.au>
2025-06-20 11:59:51 +0000

    [automatic commit] 2025-06-20 11:59:50 UTC 122.150.235.197

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -75,7 +75,9 @@ Sample Response:
 router.get('/walkers/summary', async function(req, res, next) {
   const [rows] = await pool.query(`
         SELECT u.username AS walker_username,
-            COUNT(SELECT * FROM ) AS total_ratings,
+            COUNT(
+                SELECT *
+                FROM ) AS total_ratings,
             AVG() AS average_rating,
             COUNT() AS completed_walks
         FROM WalkRequests AS w
