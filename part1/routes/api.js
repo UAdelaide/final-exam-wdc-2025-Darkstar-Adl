d70227643e88333bb3346519c15c9320de5419d8
darkstar-adl <a1926618@adelaide.edu.au>
2025-06-20 11:21:01 +0000

    [automatic commit] 2025-06-20 11:21:01 UTC 122.150.235.86

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -1,4 +1,3 @@
-var createError = require('http-errors');
 const express = require('express');
 const pool = require('../db.js');
 const router = express.Router();
@@ -30,22 +29,6 @@ router.get('/dogs', async function(req, res, next) {
         ;
     `);
     res.json(rows);
-});
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = router;
