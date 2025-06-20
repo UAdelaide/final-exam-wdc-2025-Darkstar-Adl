78e0904e17bf857e1dffff7c7dced03db714a233
darkstar-adl <a1926618@adelaide.edu.au>
2025-06-20 11:01:09 +0000

    [automatic commit] 2025-06-20 11:01:09 UTC 122.150.235.197

--- part1/Part5-Rows.sql
+++ part1/Part5-Rows.sql
@@ -1,21 +1,21 @@
 -- v2 20/06 6pm (untested, TODO: test)
 
 
-INSERT INTO users (username, email, password_hash, role) VALUES
+INSERT INTO Users (username, email, password_hash, role) VALUES
 ("alice123", "alice@example.com", "hashed123", "owner"),
 ("bobwalker", "bob@example.com", "hashed456", "walker"),
 ("carol123", "carol@example.com", "hashed789", "owner"),
 ("exampasser123", "passing@exam.com", "hashed98", "walker"),
 ("examsuccess99", "winning@exam.com", "hashed99", "owner");
 
-INSERT INTO dogs (name, size, owner_id) VALUES
+INSERT INTO Dogs (name, size, owner_id) VALUES
 ("Max", "medium", (SELECT id FROM Users WHERE username = "alice123")),
 ("Bella", "small", (SELECT id FROM Users WHERE username = "carol123")),
 ("Scooby", "large", (SELECT id FROM Users WHERE username = "examsuccess99")),
 ("Knine", "small", (SELECT id FROM Users WHERE username = "examsuccess99")),
 ("Maxwell", "medium", (SELECT id FROM Users WHERE username = "exampasser123"));
 
-INSERT INTO walk_requests (dog_id, requested_time, duration_minutes, location, status) VALUES
+INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
 ((SELECT user_id FROM Dogs WHERE name = "Max"), "2025-06-10 08:00:00", 30, "Parklands", "open"),
 ((SELECT user_id FROM Dogs WHERE name = "Bella"), "2025-06-10 09:30:00", 45, "Beachside Ave", "accepted"),
 ((SELECT user_id FROM Dogs WHERE name = "Scooby"), "2025-06-20 08:30:00", 45, "Starting Ave", "accepted"),
