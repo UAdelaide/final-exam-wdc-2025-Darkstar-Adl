9623fa04426b9573e5c3ad8a2ff343c48c5d4ee6
darkstar-adl <a1926618@adelaide.edu.au>
2025-06-20 07:39:21 +0000

    [automatic commit] 2025-06-20 07:39:21 UTC 122.150.235.86

--- starthere/app.js
+++ starthere/app.js
@@ -47,10 +47,26 @@ let db;
     const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
     if (rows[0].count === 0) {
       await db.execute(`
-        INSERT INTO books (title, author) VALUES
-        ('1984', 'George Orwell'),
-        ('To Kill a Mockingbird', 'Harper Lee'),
-        ('Brave New World', 'Aldous Huxley')
+INSERT INTO users (username, email, password_hash, role) VALUES
+("alice123", "alice@example.com", "hashed123", "owner"),
+("bobwalker", "bob@example.com", "hashed456", "walker"),
+("carol123", "carol@example.com", "hashed789", "owner"),
+("exampasser123", "passing@exam.com", "hashed98", "walker"),
+("examsuccess99", "winning@exam.com", "hashed99", "owner");
+
+INSERT INTO dogs (name, size, owner_id) VALUES
+("Max", "medium", (SELECT id FROM users WHERE username = "alice123")),
+("Bella", "small", (SELECT id FROM users WHERE username = "carol123")),
+("Scooby", "large", (SELECT id FROM users WHERE username = "examsuccess99")),
+("Knine", "small", (SELECT id FROM users WHERE username = "examsuccess99")),
+("Maxwell", "medium", (SELECT id FROM users WHERE username = "exampasser123"));
+
+INSERT INTO walk_requests (dog_id, requested_time, duration_minutes, location, status) VALUES
+((SELECT id FROM dogs WHERE name = "Max"), "2025-06-10 08:00:00", 30, "Parklands", "open"),
+((SELECT id FROM dogs WHERE name = "Bella"), "2025-06-10 09:30:00", 45, "Beachside Ave", "accepted"),
+((SELECT id FROM dogs WHERE name = "Scooby"), "2025-06-20 08:30:00", 45, "Starting Ave", "accepted"),
+((SELECT id FROM dogs WHERE name = "Knine"), "2025-06-21 01:30:00", 60, "Middle St", "open"),
+((SELECT id FROM dogs WHERE name = "Maxwell"), "2025-06-21 16:30:00", 30, "Final Blvd", "open");
       `);
     }
   } catch (err) {
