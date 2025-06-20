2fba2db6ed1f08037f49b92a7a152502c1d086a6
darkstar-adl <a1926618@adelaide.edu.au>
2025-06-20 11:17:52 +0000

    [automatic commit] 2025-06-20 11:17:51 UTC 122.150.235.197

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -33,12 +33,12 @@ router.get('/dogs', async function(req, res, next) {
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+router.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+router.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
