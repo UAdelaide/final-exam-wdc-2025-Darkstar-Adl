50f2ed523f01bb0214aba0ae93f3bec436e49e79
darkstar-adl <a1926618@adelaide.edu.au>
2025-06-20 11:00:07 +0000

    [automatic commit] 2025-06-20 11:00:07 UTC 122.150.235.86

--- part1/Part5-Rows.sql
+++ part1/Part5-Rows.sql
@@ -1,14 +1,14 @@
 -- v2 20/06 6pm (untested, TODO: test)
 
 
-INSERT INTO Users (username, email, password_hash, role) VALUES
+INSERT INTO users (username, email, password_hash, role) VALUES
 ("alice123", "alice@example.com", "hashed123", "owner"),
 ("bobwalker", "bob@example.com", "hashed456", "walker"),
 ("carol123", "carol@example.com", "hashed789", "owner"),
 ("exampasser123", "passing@exam.com", "hashed98", "walker"),
 ("examsuccess99", "winning@exam.com", "hashed99", "owner");
 
-INSERT INTO Dogs (name, size, owner_id) VALUES
+INSERT INTO dogs (name, size, owner_id) VALUES
 ("Max", "medium", (SELECT id FROM Users WHERE username = "alice123")),
 ("Bella", "small", (SELECT id FROM Users WHERE username = "carol123")),
 ("Scooby", "large", (SELECT id FROM Users WHERE username = "examsuccess99")),
