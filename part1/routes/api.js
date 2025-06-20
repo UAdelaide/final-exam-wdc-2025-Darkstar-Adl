7ca66c43bcc662e512825ae8df8c12422a8cc232
darkstar-adl <a1926618@adelaide.edu.au>
2025-06-20 08:04:19 +0000

    [automatic commit] 2025-06-20 08:04:19 UTC 122.150.235.197

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -1,7 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-/*  */
+/* api/dogs
+
+Return a list of all dogs with their size and owner's username.
+
+Sample Response:
+
+[
+  {
+    "dog_name": "Max",
+    "size": "medium",
+    "owner_username": "alice123"
+  },
+  {
+    "dog_name": "Bella",
+    "size": "small",
+    "owner_username": "carol123"
+  }
+]
+*/
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
