d3cb7691818c00b4a3d207d3db8353fb7b40cc61
darkstar-adl <a1926618@adelaide.edu.au>
2025-06-20 07:45:14 +0000

    [automatic commit] 2025-06-20 07:45:14 UTC 122.150.235.86

--- part1/app.js
+++ part1/app.js
@@ -105,7 +105,7 @@ CREATE TABLE WalkRatings (
     `);
 
     // Populate tables
-      await db.execute(`
+    await db.execute(`
 INSERT INTO users (username, email, password_hash, role) VALUES
 ("alice123", "alice@example.com", "hashed123", "owner"),
 ("bobwalker", "bob@example.com", "hashed456", "walker"),
