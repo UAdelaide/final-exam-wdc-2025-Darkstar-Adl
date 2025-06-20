8b2f3801af3bcc398f7aba3073fd0ba5d28da7e7
darkstar-adl <a1926618@adelaide.edu.au>
2025-06-20 10:58:44 +0000

    [automatic commit] 2025-06-20 10:58:44 UTC 122.150.235.86

--- part1/Part5-Rows.sql
+++ part1/Part5-Rows.sql
@@ -1,7 +1,7 @@
 -- v2 20/06 6pm (untested, TODO: test)
 
 
-INSERT INTO users (username, email, password_hash, role) VALUES
+INSERT INTO Users (username, email, password_hash, role) VALUES
 ("alice123", "alice@example.com", "hashed123", "owner"),
 ("bobwalker", "bob@example.com", "hashed456", "walker"),
 ("carol123", "carol@example.com", "hashed789", "owner"),
