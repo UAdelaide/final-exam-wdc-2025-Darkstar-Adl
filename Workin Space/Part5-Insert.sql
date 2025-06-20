161068ac6c12ea0a60631225da632d0da6d56f02
darkstar-adl <a1926618@adelaide.edu.au>
2025-06-20 07:20:59 +0000

    [automatic commit] 2025-06-20 07:20:58 UTC 122.150.235.142

--- Workin Space/Part5-Insert.sql
+++ Workin Space/Part5-Insert.sql
@@ -10,11 +10,11 @@ INSERT INTO dogs (name, size, owner_id) VALUES
 ("Bella", "small", (SELECT id FROM users WHERE username = "carol123")),
 ("Scooby", "large", (SELECT id FROM users WHERE username = "examsuccess99")),
 ("Knine", "small", (SELECT id FROM users WHERE username = "examsuccess99")),
-("Maxwell", "medium", (SELECT id FROM users WHERE username = "exampasser123")),
+("Maxwell", "medium", (SELECT id FROM users WHERE username = "exampasser123"));
 
 INSERT INTO walk_requests (dog_id, requested_time, duration_minutes, location, status) VALUES
 ((SELECT id FROM dogs WHERE name = "Max"), "2025-06-10 08:00:00", 30, "Parklands", "open"),
 ((SELECT id FROM dogs WHERE name = "Bella"), "2025-06-10 09:30:00", 45, "Beachside Ave", "accepted"),
 ((SELECT id FROM dogs WHERE name = "Scooby"), "2025-06-20 08:30:00", 45, "Starting Ave", "accepted"),
 ((SELECT id FROM dogs WHERE name = "Knine"), "2025-06-21 01:30:00", 60, "Middle St", "open"),
-((SELECT id FROM dogs WHERE name = "Maxwell"), "2025-06-21 16:30:00", 30, "Final Blvd", "open"),
+((SELECT id FROM dogs WHERE name = "Maxwell"), "2025-06-21 16:30:00", 30, "Final Blvd", "open");
