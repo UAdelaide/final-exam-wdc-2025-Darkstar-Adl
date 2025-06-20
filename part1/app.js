0bd3b517b8c7697ec2d0920cf3b285afea30f698
darkstar-adl <a1926618@adelaide.edu.au>
2025-06-20 11:16:29 +0000

    [automatic commit] 2025-06-20 11:16:29 UTC 122.150.235.83

--- part1/app.js
+++ part1/app.js
@@ -53,7 +53,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-app.use('/', indexRouter);
+app.use('/', indexRouter, req, res, next);
 app.use('/api', apiRouter);
 app.use('/users', usersRouter);
 
