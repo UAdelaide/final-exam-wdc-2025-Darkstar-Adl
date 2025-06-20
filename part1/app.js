059b91aa9c65f97659be5b2d99693e9ccafc81af
darkstar-adl <a1926618@adelaide.edu.au>
2025-06-20 08:13:41 +0000

    [automatic commit] 2025-06-20 08:13:40 UTC 122.150.235.86

--- part1/app.js
+++ part1/app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mysql = require('mysql2/promise');
 require('dotenv').config();
-var pool = require('../db.js');
 
 
 var indexRouter = require('./routes/index');
