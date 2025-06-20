2db98f3f7fe4d79fbf168b9613bf65991f8c5686
darkstar-adl <a1926618@adelaide.edu.au>
2025-06-20 08:27:31 +0000

    [automatic commit] 2025-06-20 08:27:31 UTC 122.150.235.197

--- part1/app.js
+++ part1/app.js
@@ -43,9 +43,9 @@ let db;
 
     // Connect to db
     db = await mysql.createConnection({
-      host: process.env.DB_HOST
-      user: 'root',
-      password: '',
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME
     });
 
