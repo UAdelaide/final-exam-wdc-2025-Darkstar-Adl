4e9a095e74679652770474b0ad4203d186182120
darkstar-adl <a1926618@adelaide.edu.au>
2025-06-20 04:24:36 +0000

    [automatic commit] 2025-06-20 04:24:36 UTC 122.150.235.86

--- Workin Space/Part5-Insert.sql
+++ Workin Space/Part5-Insert.sql
@@ -1,3 +1,4 @@
 INSERT INTO users (username, email, password_hash, role)
 ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker')
+('carol123', 'carol@example.com', 'hashed789', 'owner')
