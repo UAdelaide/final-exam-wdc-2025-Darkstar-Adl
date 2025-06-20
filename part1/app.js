4004d12eb6e9925b34e1224b178dbd8909f26dc4
darkstar-adl <a1926618@adelaide.edu.au>
2025-06-20 07:45:56 +0000

    [automatic commit] 2025-06-20 07:45:56 UTC 122.150.235.83

--- part1/app.js
+++ part1/app.js
@@ -126,8 +126,7 @@ INSERT INTO walk_requests (dog_id, requested_time, duration_minutes, location, s
 ((SELECT id FROM dogs WHERE name = "Scooby"), "2025-06-20 08:30:00", 45, "Starting Ave", "accepted"),
 ((SELECT id FROM dogs WHERE name = "Knine"), "2025-06-21 01:30:00", 60, "Middle St", "open"),
 ((SELECT id FROM dogs WHERE name = "Maxwell"), "2025-06-21 16:30:00", 30, "Final Blvd", "open");
-      `);
-    }
+    `);
   } catch (err) {
     console.error('There was an error in db init. Mysql might not have been started try: service mysql start', err);
   }
