f8f2640de1b5ef5da283f121d951f52b5a2deb88
darkstar-adl <a1926618@adelaide.edu.au>
2025-06-21 07:13:32 +0000

    [automatic commit] 2025-06-21 07:13:32 UTC 122.150.235.197

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -64,8 +64,8 @@ router.post('/login', async (req, res) => {
 // POST logout
 router.post('/logout', async (req, res) => {
   const { username } = req.body;
-  req.session.destroy(){
-    
+  req.session.destroy(function(error)){
+
   }
 });
 
